800432a6b29cba6d64ab8bf82b3c8609ade30c20
Jiapeng <45415693+tissten@users.noreply.github.com>
2025-09-12 17:03:30 +0800

    Update

--- gymnasium_envrionments/scripts/run_threshold_training.py
+++ gymnasium_envrionments/scripts/run_threshold_training.py
@@ -55,7 +55,7 @@ def evaluate(data_path, training_config, seed, alg_config, env, agent1, agent2, 
         #         # display=env_config.display,
         #     )
 
-        #  TD3运行的这个loop，policy_loop
+        
         # elif agent.type == "policy":
         pbe.evaluate_policy_network(
             env,
@@ -299,13 +299,13 @@ def main():
 
 
 if __name__ == "__main__":
-    # Step 1: 先单独解析 --relation_path
+    
     base_parser = argparse.ArgumentParser()
     base_parser.add_argument('--relation_path', type=str, required=True,
                             help='Path to the Python file containing the metamorphic relation')
     args, remaining_args = base_parser.parse_known_args()
 
-    # Step 2: 动态导入 relation 文件
+    
     relation_path = args.relation_path
     module_name = os.path.splitext(os.path.basename(relation_path))[0]
     spec = importlib.util.spec_from_file_location(module_name, relation_path)
