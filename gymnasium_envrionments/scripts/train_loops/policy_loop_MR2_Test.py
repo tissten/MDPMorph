e32bda2eec99de1cb60c2692510401ce0590e2fb
Jiapeng <45415693+tissten@users.noreply.github.com>
2025-09-21 18:58:21 +0800

    Update policy_loop_MR2_Test.py

--- gymnasium_envrionments/scripts/train_loops/policy_loop_MR2_Test.py
+++ gymnasium_envrionments/scripts/train_loops/policy_loop_MR2_Test.py
@@ -15,8 +15,7 @@ def evaluate_policy_network(
 ):
     
     ###########################################################
-    file_names = [f"./Metamorphic/test suite/T{i}.txt" for i in range(40, 50)]
-    # file_names = [f"/home/jil824/CARES_NEW/Metamorphic/test suite/T{i}({i * 10}).txt" for i in range(42, 43)]
+    file_names = [rf"./Metamorphic/test suite/T{i}.txt" for i in range(40, 50)]
     # para = 0
     for file_name in file_names:
         print(f"Running RL experiment with {file_name} in process {os.getpid()}...")
